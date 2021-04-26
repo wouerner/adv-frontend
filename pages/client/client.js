c9accb0d684ec944af19fa143d30912a2f64a3cb
wouerner <wouerner@protonmail.com>
2021-04-26 16:12:38 -0300

    init

---
+++ pages/client/client.js
@@ -0,0 +1,4 @@
+export default function Client() {
+  return <h1>Client</h1>
+}
+
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>1Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          1Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
